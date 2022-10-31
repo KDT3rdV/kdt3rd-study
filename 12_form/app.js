669c975234aaefd90fc39bbd52878429b3ae040d
whqngml <archangel67@naver.com>
2022-10-31 12:10:03 +0900

    study: 폼처리 학습 코드

--- 12_form/app.js
+++ 12_form/app.js
@@ -3,28 +3,53 @@ const app = express();
 const PORT = 8000;
 
 // views 설정
-app.set("view engine", "ejs"); // view engin 등록
+app.set("view engine", "ejs");
 // Middleware (미들웨어) 설정
 // : 요청(req)과 응답(res)의 중간에서 작업
-app.use("./views", express.static(__dirname + "/views")); // views 폴더 설정
+app.use("/views", express.static(__dirname + "/views"));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json()); // json형태로 데이터를 전달받음
+app.use(express.json()); // json 형태로 데이터를 전달받음
 
 // Routing - 경로 설정
-// - req(request): 요청 (클라이언트 -> 서버)
-//  -res(response): 응답 (서버 -> 클라이언트)
+// - req(request) : 요청 (클라이언트 -> 서버)
+// - res(response) : 응답 (서버 -> 클라이언트)
 app.get("/", function (req, res) {
   // GET / (http://localhost:PORT)
-  //   res.send("root page");
-  res.render("index"); // views/index.ejs 파일을 찾아서 클라이언트에게 응답
+  res.render("index", {
+    title: "폼 전송을 배워보자",
+    desc: "여기는 설명",
+    fin: "여기까지",
+  }); // views/index.ejs 파일을 찾아서 클라이언트에게 "응답"
 });
 
 app.get("/getForm", function (req, res) {
-  res.send("get 요청 응답 성공");
+  // get 요청은 req.query 객체에 폼 정보가 전달
+  console.log(req.query);
+  //   res.send("get 요청 응답 성공");
+  res.render("result", { title: "GET 요청 성공", userInfo: req.query });
 });
 
-app.get("/postForm", function (req, res) {
-  res.send("post 요청 응답 성공");
+app.post("/postForm", function (req, res) {
+  // post 요청은 req.body 객체에 폼 정보가 전달
+  console.log(req.body);
+  //   res.send("post 요청 응답 성공");
+  res.render("result", { title: "POST 요청 성공", userInfo: req.body });
+});
+
+app.get("/getForm2", function (req, res) {
+  res.send("get2 요청 응답 성공");
+  console.log(req.query);
+  res.render("result2", { title: "GET 요청 성공", userInfo: req.query });
+});
+
+app.post("/postForm2", function (req, res) {
+  res.send("post2 요청 응답 성공");
+  console.log(req.body);
+  res.render("result2", { title: "POST 요청 성공", userInfo: req.query });
+});
+
+app.post("/postForm3", function (req, res) {
+  res.send("post3 요청 응답 성공");
 });
 
 app.listen(PORT, function () {
