c71876dee5d773c14f85fcc42db72e029d3b9292
whqngml <archangel67@naver.com>
2022-11-11 12:25:14 +0900

    study: 16_sequelize

--- 15_mvc_practice/controller/Cmain.js
+++ 15_mvc_practice/controller/Cmain.js
@@ -1,7 +1,8 @@
 const login = require("../model/login");
-const userInfos = login.userInfos;
-const personal = login.userInfos.split("\n");
-console.log(login);
+const users = login.users;
+const personal = login.users.split("\n");
+const user = login.users.split("//");
+console.log("0: ", login);
 
 exports.main = (req, res) => {
   res.render("index", { title: "동적 폼 실습" });
@@ -9,10 +10,13 @@ exports.main = (req, res) => {
 
 exports.practice30 = (req, res) => {
   // console.log(login.userInfo());
-  console.log(req.body);
+  console.log("1: ", req.body);
   // console.log(login);
-  console.log(personal);
-  console.log(login.userInfos);
+  console.log("2: ", personal);
+  console.log("3: ", login.users);
+  console.log("4: ", users.length);
+  console.log("5: ", personal[0]);
+  console.log("5: ", user[0]);
 
   // DB로부터 받아온 id, pw vs. 사용자가 폼에 입력한 id, pw
   // if (login.userInfo().realId === req.body.userId && login.userInfo().realPw === req.body.userPw) {
@@ -20,5 +24,14 @@ exports.practice30 = (req, res) => {
   // } else {
   //   res.send({ isSuccess: false });
   // }
-  for (let i = 0; i < userInfos.length; i++) {}
+  for (let i = 0; i < users.length; i++) {
+    if (
+      user[3 * i + 1] === req.body.userId &&
+      user[3 * i + 2] === req.body.userPw
+    ) {
+      return res.send({ userInfo: users, isSuccess: true });
+    } else {
+      res.send({ isSuccess: false });
+    }
+  }
 };
---
+++ 16_sequelize/routes/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const controller = require("../controller/Cvisitor");
+const router = express.Router();
+
+// "경로 설정" = 라우팅
+// 기본 경로: localhost:PORT/
+
+// GET / => localhost:PORT/
+router.get("/", controller.main);
+
+// GET / => localhost:PORT/visitor
+router.get("/visitor", controller.getVisitors); // 전체조회
+
+// GET / => localhost:PORT/visitor/get
+router.get("/visitor/get", controller.getVisitor); // 하나조회
+
+// POST /visitor/write => localhost:PORT/visitor/write
+router.post("/visitor/write", controller.postVisitor); // 하나추가
+
+// PATCH /visitor/edit => localhost:PORT/visitor/edit
+router.patch("/visitor/edit", controller.patchVisitor); // 하나수정
+
+// DELETE /visitor/delete => localhost:PORT/visitor/delete
+router.delete("/visitor/delete", controller.deleteVisitor); // 하나 삭제
+
+// module.exports 를 통해서 router 등록
+// -> 다른 파일에서도 사용 가능
+module.exports = router;
